36fce1866c4256709ed255e704121d970ab4f8b8
JackdeGooyer <JackdeGooyer@gmail.com>
2022-09-07 16:41:34 -0400

    Completion of input and output reference
    Resumation of Work Commit

--- source/linear.py
+++ source/linear.py
@@ -3,55 +3,31 @@
 # DC shot noise - dark current based
 # Thernal noise - cap dependent (RC)
 
-import math
+import copy
+from typing import Dict, Union
 import pandas as pd
 import numpy as np
 import scipy.constants as spc
+import constants as c
 
 
-def amp_to_frequency(
-    current_df: pd.DataFrame,
-    capacitor: float,
-    threshold_voltage: float,
-    reset_time: float,
-    reset_jitter: float = None,
-) -> pd.DataFrame:
-    """Turns current to frequency
+def add_tdc_jitter(template: pd.DataFrame, jitter: float) -> pd.DataFrame:
+    """Add the jitter from the TDC
 
     Parameters
     ----------
-    current_df : pd.DataFrame
-        Dataframe containing currents to convert
-    capacitor : float
-        Size of Capacitor
-    threshold_voltage : float
-        Capacitor reset threshold
-    reset_time : float
-        Time to reset capacitor
-    reset_jitter : float, optional
-        Jitter on reset, by default None
-        If this is set, then it will be added, ussualy needed for noise
+    template : pd.DataFrame
+        Template for detector number and wavelengths
+    jitter : float
+        Total jitter
 
     Returns
     -------
     pd.DataFrame
-        Dataframe containing output frequencies
+        Dataframe containing formatted jitter
     """
-
-    out_df = current_df.copy(deep=True)
-
-    for column in out_df:
-        if column == "Wavelength_nm" or column == "Detector":
-            pass
-
-        # Perform Conversion
-        time_to_charge = out_df[column] / (capacitor * threshold_voltage)
-        out_df[column] = 1 / (time_to_charge + reset_time)
-
-        # Add reset jitter # TODO Change this
-        if reset_jitter is not None:
-            out_df[column] = out_df[column] + (1 / reset_jitter)
-
+    out_df = template[["Detector", "Wavelength_nm"]].copy(deep=True)
+    out_df["TDC_Noise_s^2"] = jitter
     return out_df
 
 
@@ -109,9 +85,7 @@ def add_shot_noise(
         Dataframe containing shot noise per detector
     """
 
-    total_current_per_detector = current_df.loc[
-        :, ~current_df.columns.str.contains("Wavelength_nm|Detector")
-    ].sum(axis=1)
+    total_current_per_detector = get_total_current(current_df)
     out_df = current_df.copy(deep=True)
     bandwidth = 1 / (capacitance * resistance)
     out_df["Shot_Noise_A_rms"] = np.sqrt(
@@ -121,6 +95,320 @@ def add_shot_noise(
         deep=True
     )
     return out_df
+
+
+def amp_to_frequency(
+    current_df: pd.DataFrame,
+    capacitor: float,
+    threshold_voltage: float,
+) -> pd.DataFrame:
+    """Turns current to frequency
+
+    Parameters
+    ----------
+    current_df : pd.DataFrame
+        Dataframe containing currents to convert
+    capacitor : float
+        Size of Capacitor
+    threshold_voltage : float
+        Capacitor reset threshold
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe containing output frequencies
+    """
+    out_df = current_df[["Detector", "Wavelength_nm"]].copy(deep=True)
+    out_df["Period_s"] = get_total_current(current_df) / (capacitor * threshold_voltage)
+    out_df["Frequency_Hz"] = 1 / out_df["Period_s"]
+    return out_df
+
+
+def voltage_noise_to_jitter(
+    noise: Union[pd.DataFrame, c.RecursiveDict, None],
+    current_df: pd.DataFrame,
+    capacitor: float,
+) -> Union[pd.DataFrame, c.RecursiveDict, None]:
+    """Converts voltage noise on capacitor to pulse jitter
+
+    Parameters
+    ----------
+    noise_df : Union[pd.DataFrame, c.RecursiveDict, None]
+        Voltage noise to convert
+    current_df : pd.DataFrame
+        Dataframe containing DC signal current
+    capacitor : float
+        Integrating capacitor
+
+    Returns
+    -------
+    Union[pd.DataFrame, c.RecursiveDict, None]
+        Structure containing jitter
+    """
+    out_noise = copy.deepcopy(noise)
+    out_noise = _voltage_noise_to_jitter(out_noise, current_df, capacitor)
+    return out_noise
+
+
+def _voltage_noise_to_jitter(
+    noise: Union[pd.DataFrame, c.RecursiveDict, None],
+    current_df: pd.DataFrame,
+    capacitor: float,
+) -> pd.DataFrame:
+    """Recursive function to get jitter from noise
+
+    Parameters
+    ----------
+    noise_df : Union[pd.DataFrame, c.RecursiveDict, None]
+        Voltage noise to convert
+    current_df : pd.DataFrame
+        Dataframe containing DC signal current
+    capacitor : float
+        Integrating capacitor
+
+    Returns
+    -------
+    Union[pd.DataFrame, c.RecursiveDict, None]
+        Structure containing jitter
+    """
+
+    # Check for type
+    if isinstance(noise, Dict):
+        for signal in noise:
+            noise[signal] = _voltage_noise_to_jitter(
+                noise[signal], current_df, capacitor
+            )
+        return noise
+    if noise is None:
+        return None
+
+    out_df = noise[["Detector", "Wavelength_nm"]].copy(deep=True)
+    total_current = get_total_current(current_df)
+    voltage_rate = total_current / capacitor
+    for column in noise[noise.columns.difference(["Detector", "Wavelength_nm"])]:
+        new_column = column.replace("_V_rms", "_s^2")
+        out_df[new_column] = (noise[column] ** 2) * (1 / (voltage_rate**2))
+    return out_df
+
+
+def de_voltage_noise_to_jitter(
+    noise: Union[pd.DataFrame, c.RecursiveDict, None],
+    current_df: pd.DataFrame,
+    capacitor: float,
+) -> Union[pd.DataFrame, c.RecursiveDict, None]:
+    """Converts jitter on comparator to voltage
+
+    Parameters
+    ----------
+    noise_df : Union[pd.DataFrame, c.RecursiveDict, None]
+        jitter noise to convert
+    jitter_df : pd.DataFrame
+        Dataframe containing DC signal current
+    capacitor : float
+        Integrating capacitor
+
+    Returns
+    -------
+    Union[pd.DataFrame, c.RecursiveDict, None]
+        Structure containing voltage noise
+    """
+    out_noise = copy.deepcopy(noise)
+    out_noise = _de_voltage_noise_to_jitter(noise, current_df, capacitor)
+    return out_noise
+
+
+def _de_voltage_noise_to_jitter(
+    noise: Union[pd.DataFrame, c.RecursiveDict, None],
+    current_df: pd.DataFrame,
+    capacitor: float,
+) -> pd.DataFrame:
+    """Recursive function to get voltage noise from jitter
+
+    Parameters
+    ----------
+    noise_df : Union[pd.DataFrame, c.RecursiveDict, None]
+        Voltage noise to convert
+    current_df : pd.DataFrame
+        Dataframe containing DC signal current
+    capacitor : float
+        Integrating capacitor
+
+    Returns
+    -------
+    Union[pd.DataFrame, c.RecursiveDict, None]
+        Structure containing jitter
+    """
+
+    # Check for type
+    if isinstance(noise, Dict):
+        for signal in noise:
+            noise[signal] = _de_voltage_noise_to_jitter(
+                noise[signal], current_df, capacitor
+            )
+        return noise
+    if noise is None:
+        return None
+
+    out_df = noise[["Detector", "Wavelength_nm"]].copy(deep=True)
+    total_current = get_total_current(current_df)
+    voltage_rate = total_current / capacitor
+    for column in noise[noise.columns.difference(["Detector", "Wavelength_nm"])]:
+        new_column = column.replace("_s^2", "_V_rms")
+        out_df[new_column] = np.sqrt(noise[column] * voltage_rate**2)
+    return out_df
+
+
+def integrate_current_on_capacitor(
+    current_df_dict: Union[c.RecursiveDict, pd.DataFrame, None],
+    total_current: pd.Series,
+    cap: float,
+    threshold: float,
+) -> Union[c.RecursiveDict, Dict]:
+    """Integrates current on capacitor
+
+    Parameters
+    ----------
+    current_df_dict : Union[c.RecursiveDict, pd.DataFrame, None]
+        Current to integrate into the capacitor
+    total_current : pd.Series
+        Total current being integrated device
+    cap : float
+        Capacitor size
+    threshold : float
+        Switching voltage level
+
+    Returns
+    -------
+    Union[c.RecursiveDict, Dict]
+        Voltages coresponding to currents
+    """
+    current_df_dict = copy.deepcopy(current_df_dict)
+    voltage_df_dict = _integrate_current_on_capacitor(
+        current_df_dict, total_current, cap, threshold
+    )
+    return voltage_df_dict
+
+
+def _integrate_current_on_capacitor(
+    current_dict: Union[c.RecursiveDict, pd.DataFrame, None],
+    total_current: pd.Series,
+    cap: float,
+    threshold: float,
+) -> Union[pd.DataFrame, Dict]:
+
+    # Check for type
+    if isinstance(current_dict, Dict):
+        for signal in current_dict:
+            current_dict[signal] = _integrate_current_on_capacitor(
+                current_dict[signal], total_current, cap, threshold
+            )
+        return current_dict
+    if current_dict is None:
+        return None
+
+    # Setup DataFrames
+    current_df = current_dict.copy(deep=True)
+    voltage_df = current_df[["Detector", "Wavelength_nm"]].copy(deep=True)
+    for column in current_df[
+        current_df.columns.difference(["Detector", "Wavelength_nm"])
+    ]:
+        new_column = column.replace("_A", "_V")
+        voltage_df[new_column] = np.sqrt(
+            (threshold / (2 * total_current * cap)) * current_df[column] ** 2
+        )
+    return voltage_df
+
+
+def de_integrate_current_on_capacitor(
+    voltage_df_dict: Union[c.RecursiveDict, pd.DataFrame, None],
+    total_current: pd.Series,
+    cap: float,
+    threshold: float,
+) -> Union[c.RecursiveDict, Dict]:
+    """De Integrates current on capacitor, Voltage to Current
+
+    Parameters
+    ----------
+    voltage_df_dict : Union[c.RecursiveDict, pd.DataFrame, None]
+        Voltage to de_integrate into the capacitor
+    total_current : pd.Series
+        Total current being integrated device
+    cap : float
+        Capacitor size
+    threshold : float
+        Switching voltage level
+
+    Returns
+    -------
+    Union[c.RecursiveDict, Dict]
+        Currents corresponding to voltages
+    """
+    voltage_df_dict = copy.deepcopy(voltage_df_dict)
+    current_df_dict = _de_integrate_current_on_capacitor(
+        voltage_df_dict, total_current, cap, threshold
+    )
+    return current_df_dict
+
+
+def _de_integrate_current_on_capacitor(
+    voltage_dict: Union[c.RecursiveDict, pd.DataFrame, None],
+    total_current: pd.Series,
+    cap: float,
+    threshold: float,
+) -> Union[pd.DataFrame, Dict]:
+
+    # Check for type
+    if isinstance(voltage_dict, Dict):
+        for signal in voltage_dict:
+            voltage_dict[signal] = _de_integrate_current_on_capacitor(
+                voltage_dict[signal], total_current, cap, threshold
+            )
+        return voltage_dict
+    if voltage_dict is None:
+        return None
+
+    # Setup DataFrames
+    voltage_df = voltage_dict.copy(deep=True)
+    current_df = voltage_df[["Detector", "Wavelength_nm"]].copy(deep=True)
+    for column in voltage_df[
+        voltage_df.columns.difference(["Detector", "Wavelength_nm"])
+    ]:
+        new_column = column.replace("_V", "_A")
+        current_df[new_column] = np.sqrt(
+            (1 / (threshold / (2 * total_current * cap))) * voltage_df[column] ** 2
+        )
+    return current_df
+
+
+def get_total_current(df: pd.DataFrame) -> pd.Series:
+    """Gets all current in a dataframe
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to get current from
+
+    Returns
+    -------
+    pd.DataFrame
+        Total current in detector channels
+    """
+    return df.loc[:, ~df.columns.str.contains("Wavelength_nm|Detector")].sum(axis=1)
+
+def get_total_noise(df: pd.DataFrame) -> pd.Series:
+    """Gets all current in a dataframe given independent noises
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to get noise from
+
+    Returns
+    -------
+    pd.Series
+        Total noise current in detector channels
+    """
+    return np.sqrt(df.loc[:, ~df.columns.str.contains("Wavelength_nm|Detector")].pow(2).sum(axis=1))
 
 
 # def add_reset_noise(
--- source/sim.py
+++ source/sim.py
@@ -2,6 +2,7 @@
 """
 
 from pathlib import Path
+import copy
 from oct import (
     beam_split,
     reflection_loss,
@@ -11,12 +12,23 @@ from oct import (
     combine_em_data,
 )
 from detector import convert_em_to_channels
-from linear import add_shot_noise, add_thermal_noise
 import constants as c
 import pandas as pd
 import numpy as np
 import plotly.express as px
 from laser_source import generate_source
+from linear import (
+    get_total_current,
+    get_total_noise,
+    integrate_current_on_capacitor,
+    amp_to_frequency,
+    voltage_noise_to_jitter,
+    add_shot_noise,
+    add_thermal_noise,
+    add_tdc_jitter,
+    de_integrate_current_on_capacitor,
+    de_voltage_noise_to_jitter,
+)
 
 
 class OCTSim:
@@ -63,7 +75,7 @@ class OCTSim:
     # Linear Values
     linear_cap: float = c.CAPACITOR
     reset_time: float = c.RESET_TIME
-    reset_jitter: float = c.RESET_JITTER
+    TDC_jitter: float = c.TDC_JITTER
     buffer_gain: float = c.BUFFER_GAIN
     threshold_voltage: float = c.VOLTAGE_THRESHOLD
 
@@ -72,10 +84,12 @@ class OCTSim:
     sample_ref_index: float = c.SAMPLE_REFRACTIVE_INDEX
     tissue_depth: float = c.DEFAULT_SAMPLE_DEPTH
 
-    def __init__(self):
+    def __init__(self, responsivity: pd.DataFrame = None):
         """Reads all data files into Detector Class"""
-        self.responsivity = pd.read_csv(self._responsivity_path, index_col=False)
-        self.source_power_df = pd.read_csv(self._source_path, index_col=False)
+        if responsivity is None:
+            self.responsivity = pd.read_csv(self._responsivity_path, index_col=False)
+        else:
+            self.responsivity = copy.deepcopy(responsivity)
         self.em_data = dict()
         self.reflectivity = dict()
         self.detector_data = dict()
@@ -170,7 +184,7 @@ class OCTSim:
             self.detector_area,
             include_dc_dark_current=True,
         )
-        self.detector_data["noise"] = convert_em_to_channels(
+        self.detector_data["current_noise"] = convert_em_to_channels(
             self.em_data["output"]["noise"],
             self.responsivity,
             self.detector_bias_voltage,
@@ -182,16 +196,58 @@ class OCTSim:
 
     def add_linear_noise(self):
         """Add noises from various features"""
-        self.detector_data["noise"]["shot_noise"] = add_shot_noise(
+        self.detector_data["current_noise"]["shot_noise"] = add_shot_noise(
             self.detector_data["signal"],
             resistance=self.detector_resistance,
             capacitance=self.linear_cap,
         )
-        self.detector_data["noise"]["thermal_noise"] = add_thermal_noise(
+        self.detector_data["current_noise"]["thermal_noise"] = add_thermal_noise(
             self.detector_data["signal"],
             temperature=self.temperature,
             resistance=self.detector_resistance,
             capacitance=self.linear_cap,
+        )
+        return
+
+    def integrate_current_noise(self):
+        """Turn current noise into voltage noise"""
+        self.detector_data["voltage_noise"] = dict()
+        total_current = get_total_current(self.detector_data["signal"])
+        self.detector_data["voltage_noise"] = integrate_current_on_capacitor(
+            self.detector_data["current_noise"],
+            total_current,
+            self.linear_cap,
+            self.threshold_voltage,
+        )
+        return
+
+    def perform_frequency_conversion(self):
+        """Turn current and voltage noise into freq and jitter"""
+        self.frequency_data["signal"] = amp_to_frequency(
+            self.detector_data["signal"], self.linear_cap, self.threshold_voltage
+        )
+        self.frequency_data["jitter"] = voltage_noise_to_jitter(
+            self.detector_data["voltage_noise"],
+            self.detector_data["signal"],
+            self.linear_cap,
+        )
+        self.frequency_data["jitter"]["TDC"] = add_tdc_jitter(
+            self.frequency_data["jitter"][list(self.frequency_data["jitter"])[0]],
+            self.TDC_jitter,
+        )
+        return
+
+    def back_convert_noises(self):
+        """Back propogates noise to input"""
+        self.detector_data["voltage_noise"] = de_voltage_noise_to_jitter(
+            self.frequency_data["jitter"], self.detector_data["signal"], self.linear_cap
+        )
+        total_current = get_total_current(self.detector_data["signal"])
+        self.detector_data["current_noise"] = de_integrate_current_on_capacitor(
+            self.detector_data["voltage_noise"],
+            total_current,
+            self.linear_cap,
+            self.threshold_voltage,
         )
         return
 
@@ -280,6 +336,15 @@ class OCTSim:
         # Peform Linear Measurements
         self.add_linear_noise()
 
+        # Perform Integration on Capacitor
+        self.integrate_current_noise()
+
+        # Convert to Frequencies and Jitter
+        self.perform_frequency_conversion()
+
+        # Back convert all types
+        self.back_convert_noises()
+
         # Convert to APD Detectins
 
         # Perform APD Measurements
@@ -293,7 +358,133 @@ class OCTSim:
 
 if __name__ == "__main__":
 
-    for input_power in np.arange(0, 5.2e-03, 0.1e-03):
-        DetectorSim = OCTSim()
-        DetectorSim.total_source_power = input_power
-        DetectorSim.run()
+    detector_data = pd.DataFrame(
+        columns=[
+            "Magnitude [A]",
+            "Input Power [W]",
+            "Signal Type",
+            "Capacitance [pF]",
+        ]
+    )
+    detector_snr_data = pd.DataFrame(
+        columns=[
+            "SNR",
+            "Input Power [W]",
+            "Signal Type",
+            "Capacitance [pF]",
+        ]
+    )
+
+    selected_detector = 512
+    for input_power in np.logspace(-15, 1, 100, base=10):
+        for capacitance in np.arange(0.1e-12, 1e-12, 0.1e-12):
+
+            capacitance = 1e-9
+
+            # Run Sim
+            DetectorSim = OCTSim()
+            DetectorSim.total_source_power = input_power
+            DetectorSim.linear_cap = capacitance
+            DetectorSim.run()
+
+            #  Get Signal
+            cap_data = []
+            snr_data = []
+            signal = get_total_current(
+                DetectorSim.detector_data["signal"][
+                    ["DC_Terms_A", "Cross_Terms_A", "Auto_Terms_A"]
+                ]
+            )[selected_detector]
+            cap_data.append(
+                {
+                    "Signal Magnitude [A]": np.log(signal),
+                    "Input Power [W]": np.log(input_power),
+                    "Signal Type": "Signal",
+                    "Capacitance [pF]": capacitance,
+                }
+            )
+
+            # Get Noises
+            total_noise = 0
+            for column in ["photon_shot", "shot_noise", "thermal_noise", "TDC", "rin"]:
+                noise = get_total_noise(
+                    DetectorSim.detector_data["current_noise"][column]
+                )[selected_detector]
+                total_noise = np.sqrt(np.power(total_noise, 2) + np.power(noise, 2))
+                cap_data.append(
+                    {
+                        "Signal Magnitude [A]": np.log(noise),
+                        "Input Power [W]": np.log(input_power),
+                        "Signal Type": column,
+                        "Capacitance [pF]": capacitance,
+                    }
+                )
+                snr_data.append(
+                    {
+                        "SNR": 20 * np.log(signal / noise),
+                        "Input Power [W]": np.log(input_power),
+                        "Signal Type": column,
+                        "Capacitance [pF]": capacitance,
+                    }
+                )
+            cap_data.append(
+                {
+                    "Signal Magnitude [A]": np.log(total_noise),
+                    "Input Power [W]": np.log(input_power),
+                    "Signal Type": "Total Noise",
+                    "Capacitance [pF]": capacitance,
+                }
+            )
+            snr_data.append(
+                {
+                    "SNR": 20 * np.log(signal / total_noise),
+                    "Input Power [W]": np.log(input_power),
+                    "Signal Type": "Total Noise",
+                    "Capacitance [pF]": capacitance,
+                }
+            )
+            detector_data = pd.concat([detector_data, pd.DataFrame(cap_data)])
+            detector_snr_data = pd.concat([detector_snr_data, pd.DataFrame(snr_data)])
+
+            break  # Remove for multi cap
+
+    detector_data["Capacitance [pF]"] = detector_data["Capacitance [pF]"] * 1e12
+    fig = px.line(
+        detector_data,
+        x="Input Power [W]",
+        y="Signal Magnitude [A]",
+        color="Signal Type",
+    )
+    fig.show()
+    fig = px.line(
+        detector_snr_data,
+        x="Input Power [W]",
+        y="SNR",
+        color="Signal Type",
+    )
+    fig.show()
+    # fig = px.scatter_3d(
+    #     detector_data,
+    #     x="Input Power [W]",
+    #     z="Signal Magnitude [A]",
+    #     y="Capacitance [pF]",
+    #     color="Signal Type",
+    # )
+    # fig.show()
+
+    # fig = px.scatter_3d(
+    #     detector_snr_data,
+    #     x="Input Power [W]",
+    #     z="SNR",
+    #     y="Capacitance [pF]",
+    #     color="Signal Type",
+    # )
+    # fig.show()
+
+    # Plot SNR versus signal 2d, SNR, input power, for a particular capacitance
+    # Start of input
+
+    # Number of bits in the counter -- add this
+    # N = 10
+    # time LSB = 1/framerate / 2^(bits in counter)
+    # seconds^2 quantization = time LSB^2 / 12
